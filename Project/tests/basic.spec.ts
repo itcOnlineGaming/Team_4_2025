4a82f1cba4e8b8976f38118037e6b4b595312ac1
Dylan Crimmings <c00273675@setu.ie>
2025-11-11 16:54:13 +0000

    #19 - adapter-static and defined base path

--- Project/tests/basic.spec.ts
+++ Project/tests/basic.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
+import { base } from '$app/paths';
 
 test('homepage loads', async ({ page }) => {
-  await page.goto('http://localhost:5173');
+  await page.goto(`http://localhost:5173${base}`);
   await expect(page).toHaveTitle(/Task Calendar|Team 4|Calendar/i);
 });
